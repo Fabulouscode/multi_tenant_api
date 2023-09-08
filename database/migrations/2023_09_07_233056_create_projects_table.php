e42fd190829e2847e5b1ad3d6015142abcb2f3b0
fabian.ulagh <fabian.ulagh@gloverapp.co>
2023-09-08 13:14:01 +0100

    update migration

--- database/migrations/2023_09_07_233056_create_projects_table.php
+++ database/migrations/2023_09_07_233056_create_projects_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->uuid('uuid')->unique();
             $table->string('name', 15);
             $table->foreignId('tenant_id')->constrained();
             $table->timestamps();
